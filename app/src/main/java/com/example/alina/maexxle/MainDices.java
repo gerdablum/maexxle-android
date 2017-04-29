f41b4c0e36a876a6c30591d32cac96496092f5fe
Alina Jaud <weihnachtenist@baldmama.de>
2017-04-29 10:30:42 +0200

    added shaking mechanism to throw dices

--- app/src/main/java/com/example/alina/maexxle/MainDices.java
+++ app/src/main/java/com/example/alina/maexxle/MainDices.java
@@ -1,6 +1,8 @@
 package com.example.alina.maexxle;
 
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,15 +14,18 @@ import android.os.Vibrator;
  */
 public class MainDices extends AppCompatActivity {
 
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Shaker mShakeDetector;
 
-    Button showHide;
-    Button throwing;
-    ImageView diceImage1;
-    ImageView diceImage2;
-    CheckBox checkVibration;
-    boolean isVibratorOn = true;
-    Dices dices;
-    Vibrator v;
+    private Button showHide;
+    private Button throwing;
+    private ImageView diceImage1;
+    private ImageView diceImage2;
+    private CheckBox checkVibration;
+    private boolean isVibratorOn = true;
+    private Dices dices;
+    private Vibrator v;
 
     /**
      * Würfel anzeigen/verbergen
@@ -125,5 +130,32 @@ public class MainDices extends AppCompatActivity {
             }
         });
 
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new Shaker();
+        mShakeDetector.setOnShakeListener(new Shaker.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+				throwDices();
+                System.out.println("Phone was shaked");
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 }
